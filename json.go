b9154bdbad87a0b8f0072778a8f6dfada82b6338
Haski007 <haski007001@gmail.com>
2021-10-25 12:25:32 +0300

    added write and read json methods

---
+++ json.go
@@ -0,0 +1,25 @@
+package files
+
+// ReadJson - here everything is clear
+func ReadJson(path string, dest interface{}) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("read file err: %s", err)
+	}
+
+	if err := json.Unmarshal(data, &dest); err != nil {
+		return fmt.Errorf("json unmarshall file [%s] data err: %s", path, err)
+	}
+
+	return err
+}
+
+// WriteJson - here everything is clear
+func WriteJson(file string, data interface{}) error {
+	newData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return fmt.Errorf("[json.MarshallIndent] err: %s", err)
+	}
+
+	return ioutil.WriteFile(file, newData, os.ModePerm)
+}
